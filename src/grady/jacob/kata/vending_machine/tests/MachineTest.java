c31cdf559ed900c4f4c428c2a7037fea1e013778
jgrady <jgrady@iowai.org>
2016-06-26 15:57:28 -0500

    Machine rejects coin penny test passes.

--- src/grady/jacob/kata/vending_machine/tests/MachineTest.java
+++ src/grady/jacob/kata/vending_machine/tests/MachineTest.java
@@ -12,12 +12,14 @@ import grady.jacob.kata.vending_machine.main.Machine;
 public class MachineTest {
 
 	private Machine Machine = new Machine();
-	
+
+	private ArrayList<Double> pennyValues = new ArrayList<Double>();
 	private ArrayList<Double> nickelValues = new ArrayList<Double>();
 	private ArrayList<Double> dimeValues = new ArrayList<Double>();
 	private ArrayList<Double> quarterValues = new ArrayList<Double>();
 	
 	public MachineTest() {
+		this.pennyValues = new ArrayList<Double>(Arrays.asList(2.5, 19.05, 1.52));
 		this.nickelValues = new ArrayList<Double>(Arrays.asList(5.0, 21.21, 1.95));
 		this.dimeValues = new ArrayList<Double>(Arrays.asList(2.268, 17.91, 1.35));
 		this.quarterValues = new ArrayList<Double>(Arrays.asList(5.67, 24.26, 1.75));
@@ -36,5 +38,10 @@ public class MachineTest {
 	@Test
 	public void machineAcceptsCoinQuarter() {
 		assertTrue(Machine.insertCoin(this.quarterValues));
+	}
+	
+	@Test
+	public void machineRejectsCoinPenny() {
+		assertFalse(Machine.insertCoin(this.pennyValues));
 	}
 }
